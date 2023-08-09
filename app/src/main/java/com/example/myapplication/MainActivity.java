eebc1c0500fa569d22a86e968addc087667f3f2a
RocketMan2k21 <82111778+RocketMan2k21@users.noreply.github.com>
2023-08-09 11:42:44 +0300

    hgjhg

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
@@ -11,6 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -21,6 +24,7 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.lang.reflect.Array;
 
@@ -36,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
     int give_cur;
     int take_cur;
+    String amount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
         myGetText = findViewById(R.id.editTextTextPersonName3);
         myGiveText = findViewById(R.id.editTextTextPersonName);
-        TextView myError = findViewById(R.id.textView3);
-
+        myGetText.setEnabled(false);
         Button exchangeBtn = findViewById(R.id.button4);
 
         requestQueue = Volley.newRequestQueue(this);
@@ -73,7 +77,44 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
+                take_cur = 1;
+            }
+        });
 
+        give_sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                give_cur = i;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                give_cur = 2;
+            }
+        });
+
+        myGiveText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                double doubleValue = 0;
+                if (s != null) {
+                    try {
+                        doubleValue = Double.parseDouble(s.toString().replace(',', '.'));
+                    } catch (NumberFormatException e) {
+                        //Error
+                    }
+                }
+                amount = String.valueOf(doubleValue);
             }
         });
 
@@ -81,21 +122,20 @@ public class MainActivity extends AppCompatActivity {
         exchangeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String amount = String.valueOf(myGiveText.getText());
+                amount = String.valueOf(myGiveText.getText());
                 if(amount.isEmpty()){
-                    myError.setText("Please enter a valid amount");
+                    Toast.makeText(MainActivity.this, "Please enter a valid amount", Toast.LENGTH_SHORT).show();;
                     amount = "0";
                     myGiveText.setText(amount);
                 }
-                myError.setText("");
-                jsonParse(amount);
+                jsonParse(amount, currensies[give_cur], currensies[take_cur]);
             }
         });
 
     }
-    private void jsonParse(String amount){
+    private void jsonParse(String amount, String give, String take){
 
-        String url = "https://v6.exchangerate-api.com/v6/5288a6e8f5171e3f971c1a97/pair/USD/UAH/" + amount;
+        String url = "https://v6.exchangerate-api.com/v6/5288a6e8f5171e3f971c1a97/pair/" + give + "/" + take +"/" + amount;
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>() {
